9c60e57e0d65514b33ab58bbf2dd1aa09ea77105
Kushal7894 <kushalehuddar@gmail.com>
2024-05-12 21:36:22 +0530

    Add files via upload

---
+++ Kushal/scrpit.js
@@ -0,0 +1,36 @@
+const url =
+	'https://api.openweathermap.org/data/2.5/weather';
+const apiKey =
+	'619e80b40a03a827b8a40ef378efed95';
+
+$(document).ready(function () {
+	weatherFn('');
+});
+
+async function weatherFn(cName) {
+	const temp =
+		`${url}?q=${cName}&appid=${apiKey}&units=metric`;
+	try {
+		const res = await fetch(temp);
+		const data = await res.json();
+		if (res.ok) {
+			weatherShowFn(data);
+		} else {
+			alert('City not found. Please try again.');
+		}
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+}
+
+function weatherShowFn(data) {
+	$('#city-name').text(data.name);
+
+	$('#temperature').
+		html(`${data.main.temp}°C`);
+	$('#description').
+		text(data.weather[0].description);
+	$('#wind-speed').
+		html(`Wind Speed: ${data.wind.speed} m/s`);
+	$('#weather-info').fadeIn();
+}
